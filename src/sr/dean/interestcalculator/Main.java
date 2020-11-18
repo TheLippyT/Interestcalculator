0667b3d4869f3ad192d936c35ca19e3eeb1bd89d
TheLippyT <dev.martodikromo99@gmail.com>
2020-11-17 21:46:59 -0300

    finalized

--- src/sr/dean/interestcalculator/Main.java
+++ src/sr/dean/interestcalculator/Main.java
@@ -5,23 +5,37 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    double verkoopprijs,aanbetaling,financieringsbedrag,rente,totaleRente,totaalPlusRente,maandelijkseAflossing, looptijd;
+	    double verkoopprijs,aanbetaling,financieringsbedrag,rente,totaleRente,totaalPlusRente,maandelijkseAflossing,looptijdInMaanden,looptijdInJaren;
 
 	    Scanner scanner = new Scanner(System.in);
 
         System.out.println("insert verkoopprijs:");
-	    verkoopprijs = scanner.nextFloat();
+	    verkoopprijs = scanner.nextDouble();
 
         System.out.println("insert looptijd:");
-        looptijd = scanner.nextFloat();
+        looptijdInJaren = scanner.nextDouble();
 
         System.out.println("insert aanbetaling:");
-        aanbetaling = scanner.nextFloat();
+        aanbetaling = scanner.nextDouble();
 
-        System.out.println("insert looptijd");
+        System.out.println("insert jaarlijkse rente:");
+        rente = scanner.nextDouble();
 
-        simpleInterest = (principle * numberOfYears * rate) /100;
+        financieringsbedrag = verkoopprijs - aanbetaling;
+        totaleRente = (financieringsbedrag*(rente/100))*looptijdInJaren;
+        totaalPlusRente = totaleRente + financieringsbedrag;
+        looptijdInMaanden = looptijdInJaren * 12;
+        maandelijkseAflossing = totaalPlusRente/looptijdInMaanden;
 
-        System.out.println("the interest amount = " + simpleInterest);
+        System.out.println("looptijd in jaren: "+looptijdInJaren);
+        System.out.println("looptijd in maanden: "+looptijdInMaanden);
+        System.out.println("verkoopprijs: "+verkoopprijs);
+        System.out.println("aanbetaling: " +aanbetaling);
+        System.out.println("finacieringsbedrag: " +financieringsbedrag);
+        System.out.println("rente: "+rente);
+        System.out.println("totaal rente: "+totaleRente);
+        System.out.println("totaal inclusief rente: "+totaalPlusRente);
+        System.out.println("maandelijkse aflossing: "+maandelijkseAflossing);
+
     }
 }
